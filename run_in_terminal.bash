26d3187758fc36932b26588fe56af3c2b95236da
Massimiliano Novelli <max.novelli@ess.eu>
2021-12-03 10:15:51 +0100

    updated conda environment and script to run it locally in terminal

--- run_in_terminal.bash
+++ run_in_terminal.bash
@@ -3,5 +3,5 @@
 #
 clear
 
-PSS_DEBUG=1 PSS_MONGODB_URL="mongodb://127.0.0.1:27017" PSS_DATABASE="pss_test" PSS_PORT="8000" python main.py
+PSS_DEBUG=1 PSS_MONGODB_URL="mongodb://127.0.0.1:27017" PSS_DATABASE="pss_test" PSS_PORT="8000" PSS_DEPLOYMENT="pss-local-test" python main.py
 
